4ba6c4c89eae9837c8532250c63ddf0faee09c53
npp97 <junhui@live.com>
2016-10-08 18:56:46 +0800

    work version

--- data_extr/main.R
+++ data_extr/main.R
@@ -1,33 +1,49 @@
 library(ncdf4);
 library(raster);
 library(chron);
-library(readxl);
+rotate <- function(x) t(apply(x, 2, rev));
 
-path0 <- "d://cru.ts//nc"
+path0 <- "d://cru.ts//nc";
 setwd(path0);
-flst <- dir()
+flst <- dir();
 
-path1 <- "d://cru.ts//expdata"
-fn_to_read <- read_excel(paste(path1, "exp"), sheet = 1);
-lo_to_ex<-data.frame(lat=,lon=,year_search=)
+path1 <- "d://cru.ts//";
+lo_to_ex <- read.csv(paste(path1, "meta_info.csv", sep = ''),colClasses = c("character","numeric","numeric","numeric"));
 
-ncdata <- nc_open(flst[1]);
-data <- ncvar_get(ncdata);
+i3 <- which((!is.na(lo_to_ex$lat))& (lo_to_ex$year<=2014));
+lo_to_ex2 <- lo_to_ex[i3,];
 
-year_search<-c(1989,2001)
-years <-as.numeric(as.character(years(as.Date(ncdata$dim[[3]]$vals, origin = '1900-01-01'))))
-ii <- which(years %in% year_search)
-data_brick <- data[,,ii]
+outp <- as.data.frame(matrix(0, ncol = 2 * length(flst) + 1, nrow = length(lo_to_ex2$id)));
+names(outp) <- c("id", paste(rep(substr(flst, 22, 24),each=2), rep(c('_mean', "_sum"),length(flst)), sep = ''));
+row.names(outp) <- lo_to_ex2$id;
+outp$id <- lo_to_ex2$id;
 
-#firstly,rotate layers and build a brick then extract
-#extent(data_brick)<-c(-90,90,180,-180)
-bba <- raster(rotate(rotate(rotate(data[,, ii[1]]))))
-extent(bba) <- c(-180, 180, -90, 90)
-for (i in 2:length(ii)) {
-    ccc <- raster(rotate(rotate(rotate(data[,, ii[i]]))));
-    extent(ccc) <- c(-180, 180, -90, 90)
-    bba <- stack(bba, ccc);
+for (ijk in 1:length(flst)) {
+
+    ncdata <- nc_open(flst[ijk]);
+    data <- ncvar_get(ncdata);
+    nc_close(ncdata);
+
+    years <- as.numeric(as.character(years(as.Date(ncdata$dim[[3]]$vals, origin = '1900-01-01'))))
+    
+    for (ij in 1:length(lo_to_ex2$id)) {
+
+        year_search <- lo_to_ex2$year[ij]
+        ii <- which(years %in% year_search)
+        if(length(ii)<1) break;
+        bba <- raster(rotate(rotate(rotate(data[,, ii[1]]))))
+        extent(bba) <- c(-180, 180, -90, 90)
+        for (i in 2:length(ii)) {
+            ccc <- raster(rotate(rotate(rotate(data[,,ii[i]]))));
+            extent(ccc) <- c(-180, 180, -90, 90)
+            bba <- stack(bba, ccc);
+        }
+
+        data_cal <- as.numeric(extract(bba, data.frame(x = lo_to_ex2$lon[ij], y = lo_to_ex2$lat[ij])), method = 'bilinear')
+        if(is.na(mean(data_cal, na.rm = T))) break;
+        outp[ij, c(2 * ijk, 2 * ijk + 1)] = c(mean(data_cal, na.rm = T), sum(data_cal, na.rm = T))
+    }
+
 }
 
-data_cal <- as.numeric(extract(bba, data.frame(x = 11, y = 12)))
-
+write.csv(outp,'outp.csv')
